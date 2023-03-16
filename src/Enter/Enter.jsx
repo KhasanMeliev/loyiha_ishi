ccad21080ce93577d2f401e9e6944109bc4a1b80
Khushnud <khushnudmeliev@gmail.com>
2023-03-16 22:22:52 +0500

    bugs fixed

--- src/Enter/Enter.jsx
+++ src/Enter/Enter.jsx
@@ -4,20 +4,18 @@ import { ButtonWrapper } from "./styles";
 
 const Enter = () => {
   return (
-    <div>
+    <ButtonWrapper>
       <h1 style={{ fontSize: "40px", color: "white" }}>Mavzuni tanlang</h1>
-      <ButtonWrapper>
-        <Link to="/square">
-          <button>Kvadrat</button>
-        </Link>
-        <Link to="/rectangle">
-          <button>To'g'ri To'rtburchak</button>
-        </Link>
-        <Link to="/quadratic">
-          <button>Kvadrat Tenglama</button>
-        </Link>
-      </ButtonWrapper>
-    </div>
+      <Link to="/square">
+        <button>Kvadrat</button>
+      </Link>
+      <Link to="/rectangle">
+        <button>To'g'ri To'rtburchak</button>
+      </Link>
+      <Link to="/quadratic">
+        <button>Kvadrat Tenglama</button>
+      </Link>
+    </ButtonWrapper>
   );
 };
 
--- src/Enter/styles.js
+++ src/Enter/styles.js
@@ -3,12 +3,14 @@ import styled from "styled-components";
 export const ButtonWrapper = styled.div`
   width: 100%;
   display: flex;
+  align-items: center;
+  justify-content: center;
   flex-direction: column;
   gap: 10px;
   margin-top: 50px;
 
   button {
-    width: 100%;
+    width: 300px;
     padding: 15px;
     background-color: white;
     border: none;
